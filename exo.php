a093d0c06be51be4169f63e55232ec8e7f64e2aa
ndeyefatoundiour <ndeyefatoundiour530@gmail.com>
2026-06-30 23:23:58 +0000

    declaration tableau

---
+++ exo.php
@@ -0,0 +1,35 @@
+<?php
+
+$categories = [
+
+   0 =>      [
+            "code" => "0000",
+            "nom" => "categorie1",
+            "produits" => [
+                  0 => [
+                    "nom" => "chips",
+                    "reference" => "1345tf",
+                    "prix" => 1000,
+                    "quantite" => 50 
+                  ],
+                  1 => [
+                    "nom" => "lait",
+                    "reference" => "9876gv",
+                    "prix" => 1500,
+                    "quantite" => 30 
+                  ]
+            ]
+         ],
+   1 =>      [
+            "code" => "1111",
+            "nom" => "categorie2",
+            "produits" => []
+         ]
+];
+
+
+
+
+
+
+?>
